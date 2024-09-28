e78435a8ea7959544de59eeaa5ca0a982808c532
Jardarr <dmitrynordmann@gmail.com>
2024-09-28 11:46:05 +0300

    add: security attention on Todo page + height

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TodoApp from "./components/TodoApp";
 export default function Home() {
 	return (
 		<>
-			<div className="h-custom w-full dark:text-gray-300 flex flex-col items-center sm:justify-center">
+			<div className="h-full w-full dark:text-gray-300 flex flex-col items-center sm:justify-center">
 				<div className="mb-2 sm:text-5xl text-center">
 					<span className="text-red-500/50 dark:text-red-500/30">
 						Site as a service
